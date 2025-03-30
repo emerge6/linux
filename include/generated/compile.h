8c0646c688ae2e0346ee3bed304cb4cbcd1c214e
emerge6 <timurpustovoit@gmail.com>
2025-03-30 09:05:10 +0300

    First kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,4 @@
+#define UTS_MACHINE		"x86_64"
+#define LINUX_COMPILE_BY	"root"
+#define LINUX_COMPILE_HOST	"gentoo"
+#define LINUX_COMPILER		"gcc (Gentoo 14.2.1_p20241221 p7) 14.2.1 20241221, GNU ld (Gentoo 2.44 p1) 2.44.0"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "6.14.0-openbase-src-custom"
